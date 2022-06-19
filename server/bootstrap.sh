bd1f3dc1f3e61164af93bf19f8626451818601aa
Jonathan Bachman <bchmn@users.noreply.github.com>
2022-06-19 10:48:55 +0300

    Update bootstrap.sh

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -3,5 +3,6 @@ set -e
 
 python3 -m venv .venv
 source .venv/bin/activate
+pip install -u pip
 pip install -r requirements.txt
 pip install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
